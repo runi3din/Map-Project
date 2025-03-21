c3f52e40a0423565260cfbceabc6998ff8f75392
runi3din <rani3din@gmail.com>
2025-03-21 10:59:53 +0330

    UI Completed

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>7Map</title>
+    <link href="favicon.png" rel="shortcut icon" type="image/png">
+    <link rel="stylesheet" href="./assets/css/styles.css" />
+
+    <link rel="stylesheet" href="./assets/css/leaflet.css">
+    <script src="./assets/js/leaflet.js"></script>
+</head>
+
+<body>
+    <div class="main">
+        <div class="head">
+            <input type="text" id="search" placeholder="دنبال کجا می گردی؟">
+        </div>
+        <div class="mapContainer">
+            <div id="map"></div>
+        </div>
+    </div>
+
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+     crossorigin=""></script>
+
+    <script>
+
+        const map = L.map('map').setView([35.7, 51.4], 11);
+
+        const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+    </script>
+
+</body>
+
+</html>
